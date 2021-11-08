41258b7538fa7a5c8e1267a34f9e217d41cc6ae5
Lauren <deitz.lauren@gmail.com>
2021-11-08 11:34:10 -0500

    docs update

---
+++ docs/_build/jupyter_execute/example.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Example usage
+# 
+# To use `cipher_lmd2231` in a project:
+
+# In[1]:
+
+
+import cipher_lmd2231
+
+print(cipher_lmd2231.__version__)
+
