4a9d36a5b697f31ab1ff5e71a45da24eb7972cf7
Lauren <deitz.lauren@gmail.com>
2021-11-07 20:00:42 -0500

    adding tests

--- tests/test_cipher_lmd2231.py
+++ tests/test_cipher_lmd2231.py
@@ -1,1 +1,12 @@
 from cipher_lmd2231 import cipher_lmd2231
+import pytest
+
+@pytest.mark.parametrize('example,expected',[
+    ('OneSingleWord','TsjXnslqjbtwi'),
+    ('lowercase','qtBjwhfxj'),
+    ('UPPERCASE','ZUUJWHFXJ'),
+    ('A sentence with spaces.','F xjsyjshj Bnym xufhjx.')
+])
+def test_cipher_multiple_values(example, expected):
+    actual = cipher_lmd2231.cipher(example,5)
+    assert actual == expected
